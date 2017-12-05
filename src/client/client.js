3a2b6b7437390b52d05321b788dd2ebbe08a0b83
Ke Yang <yangke687@gmail.com>
2017-12-05 22:11:02 +0800

    Render Home component on client side

--- src/client/client.js
+++ src/client/client.js
@@ -1,2 +1,7 @@
 // startup point of the client side application
-console.log('Hi ,there');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home';
+
+ReactDOM.hydrate(<Home />, document.querySelector('#root'));
--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>I'm the home component</div>;
+  
+  const handleClick = function(){
+    console.log('Hi, there');
+  };
+
+  return (
+    <div>
+      I'm the home component
+      <button onClick={handleClick}>Press Me</button>
+    </div>
+  );
 };
 
 export default Home;
