b66798a02b9fb9b0bb967a4be9d58321c9e85114
Ke Yang <yangke687@gmail.com>
2017-12-06 22:27:58 +0800

    not include node modules into server side bundle.js

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
   /** Inform webpack that we're buiding a bundle 
@@ -19,6 +20,12 @@ const config = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
+  /** anything that inside the node moduels 
+   * will not be included into our server side bundle.js 
+   * */
+  externals: [
+    webpackNodeExternals(),
+  ]
 };
 
 module.exports = merge(baseConfig, config);
