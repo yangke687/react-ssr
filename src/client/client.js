deb8868dc20f32dc470c428483dcc96b17769da2
Ke Yang <yangke687@gmail.com>
2018-01-12 21:55:35 +0800

    Import redux to client side

--- src/client/client.js
+++ src/client/client.js
@@ -5,9 +5,16 @@ import ReactDOM from 'react-dom';
 //import Home from './components/Home';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import Provider from 'react-redux';
+
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 ReactDOM.hydrate(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>
 , document.querySelector('#root'));
