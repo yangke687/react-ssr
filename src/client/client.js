4574b8b26efa74ba2615947c379e0d42076e44fe
Ke Yang <yangke687@gmail.com>
2018-01-12 23:58:28 +0800

    import reducers into client side

--- src/client/client.js
+++ src/client/client.js
@@ -7,7 +7,8 @@ import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import Provider from 'react-redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
 
 const store = createStore(reducers, {}, applyMiddleware(thunk));
 
