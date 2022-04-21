2fc8f82797607fd66154154c0d8de68a0e2f42ca
Ke Yang <yangke687@gmail.com>
2022-04-21 20:12:13 +0800

    Fix comment typo

--- webpack.server.js
+++ webpack.server.js
@@ -4,17 +4,17 @@ const baseConfig = require('./webpack.base.js');
 const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
-  /** Inform webpack that we're buiding a bundle 
+  /** Inform webpack that we're building a bundle
    * for nodeJS, rather than for the browser
    */
   target: 'node',
-  /** Tell webpack the root file of our server 
+  /** Tell webpack the root file of our server
    * application
    */
   entry: './src/index.js',
   /**
    * Tell webpack where to put the output file
-   * that is generated 
+   * that is generated
    */
   output: {
     filename: 'bundle.js',
